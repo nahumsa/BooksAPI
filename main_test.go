ae99f106307b12c24c93021017d5e80ba464321a
nahumsa <nahumsaa@gmail.com>
2020-11-25 16:36:59 -0300

    Added Author and Book search test

--- main_test.go
+++ main_test.go
@@ -68,3 +68,66 @@ func TestPOSTBook(t *testing.T) {
 	assert.True(t, string(readBuf) == body)
 
 }
+
+func TestBookSearch(t *testing.T) {
+	// Setup database and Grab our router
+	setupTestDatabase(false)
+	router := setupRouter()
+
+	// Perform a GET request with that handler.
+	w := performRequest(router, "GET", "/books/id/1")
+
+	// Assert we encoded correctly,
+	// the request gives a 200
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	// Test body response
+	body := `{"data":{"id":1,"title":"Gödel, Escher, Bach: an Eternal Golden Braid","author":"Douglas Hofstader"}}`
+	readBuf, _ := ioutil.ReadAll(w.Body)
+	assert.True(t, string(readBuf) == body)
+
+}
+
+func TestAuthorSearch(t *testing.T) {
+	// Setup database and Grab our router
+	setupTestDatabase(false)
+	router := setupRouter()
+
+	// Perform a GET request with that handler.
+	w := performRequest(router, "GET", "/books/author/Douglas Hofstader")
+
+	// Assert we encoded correctly,
+	// the request gives a 200
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	// Test body response
+	body := `{"data":[{"id":1,"title":"Gödel, Escher, Bach: an Eternal Golden Braid","author":"Douglas Hofstader"}]}`
+	readBuf, _ := ioutil.ReadAll(w.Body)
+	assert.True(t, string(readBuf) == body)
+
+}
+
+// func TestDELETEBook(t *testing.T) {
+
+// 	// Expected response
+// 	body := `{"data":{"id":1,"title":"Gödel, Escher, Bach: an Eternal Golden Braid","author":"Douglas Hofstader"}}`
+// 	w := httptest.NewRecorder()
+
+// 	// Setup database and Grab our router
+// 	setupDatabase(true)
+// 	router := setupRouter()
+
+// 	values := map[string]string{"title": "Gödel, Escher, Bach: an Eternal Golden Braid", "author": "Douglas Hofstader"}
+
+// 	jsonValue, _ := json.Marshal(values)
+// 	req, _ := http.NewRequest("POST", "/books", bytes.NewBuffer(jsonValue))
+// 	req.Header.Set("X-Custom-Header", "myvalue")
+// 	req.Header.Set("Content-Type", "application/json")
+
+// 	router.ServeHTTP(w, req)
+// 	assert.Equal(t, http.StatusOK, w.Code)
+
+// 	readBuf, _ := ioutil.ReadAll(w.Body)
+// 	assert.True(t, string(readBuf) == body)
+
+// }
